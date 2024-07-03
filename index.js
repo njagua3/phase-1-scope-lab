a7e418666ae6237826269ebf68d9467f2f5b5d6b
njagua <njagua.gma@gmail.com>
2024-07-03 08:53:25 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+var customerName = 'bob'
+
+//How to uppercase a variable 2 examples
+
+//function upperCaseCustomerName(){
+//    if (customerName === 'bob') {
+///        customerName = customerName.toUpperCase();
+//}
+//}
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'kevin';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'john';
+}
